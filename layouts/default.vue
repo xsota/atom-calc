275676683579b489cdd9d63fd61a7a74efe337c9
xsota <git@xsota.com>
2021-03-09 13:29:59 +0900

    first

---
+++ layouts/default.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-app dark>
+    <v-app-bar :clipped-left="clipped" fixed app>
+      <v-icon>mdi-calculator</v-icon>
+      <v-toolbar-title v-text="title" />
+      <v-spacer />
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-main>
+    <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
+      <v-list>
+        <v-list-item @click.native="right = !right">
+          <v-list-item-action>
+            <v-icon light> mdi-repeat </v-icon>
+          </v-list-item-action>
+          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-footer :absolute="!fixed" app>
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'ATOM Reward Calculator',
+    }
+  },
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,146 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12">
+      <v-card>
+        <v-card-title class="headline">試算</v-card-title>
+        <div class="d-flex">
+          <v-text-field v-model="amount" label="保持ATOM" class="mx-5" />
+          <v-text-field v-model="fee" label="fee" class="mr-5" />
+          <v-text-field v-model="apy" label="apy" class="mr-5" />
+        </div>
+        <v-card-text>
+          <v-data-table
+            :headers="headers"
+            :items="items"
+            :items-per-page="366"
+            :item-class="itemClass"
+            hide-default-footer
+          >
+          </v-data-table>
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      fee: 0.0075,
+      amount: 1000,
+      headers: [
+        {
+          text: '周期',
+          align: 'start',
+          sortable: false,
+          value: 'period',
+        },
+        {
+          text: '30日でかかる手数料',
+          align: 'start',
+          sortable: false,
+          value: 'totalFee',
+        },
+        {
+          text: '周期毎ATOM',
+          align: 'start',
+          sortable: false,
+          value: 'rewardByPeriod',
+        },
+        {
+          text: '30日で得るATOM',
+          align: 'start',
+          sortable: false,
+          value: 'monthRewardAtom',
+        },
+        {
+          text: '365日でかかる手数料',
+          align: 'start',
+          sortable: false,
+          value: 'totalFeeYear',
+        },
+        {
+          text: '365日で得るATOM',
+          align: 'start',
+          sortable: false,
+          value: 'yearRewardAtom',
+        },
+      ],
+      apy: 10.66,
+    }
+  },
+  computed: {
+    items() {
+      const items = [
+        {
+          period: '無し',
+          totalFee: 0,
+          rewardByPeriod: 0,
+          monthRewardAtom:
+            (this.amount * (this.apy / 365 / 24) * (24 * 30)) / 100,
+          yearRewardAtom: (this.amount * this.apy) / 100,
+        },
+      ]
+
+      for (let day = 1; day <= 30; day++) {
+        const reinvestCount = 30 / day
+        const periodInterestRate =
+          ((this.apy / 365 / 24) * (24 * day)) / 100 + 1
+        const rewardByPeriod = this.amount * (periodInterestRate - 1)
+        const totalFee = this.fee * reinvestCount
+
+        const composedInterestRate = periodInterestRate ** reinvestCount
+
+        const monthRewardAtom =
+          this.amount * composedInterestRate - totalFee - this.amount
+
+        const reinvestCountYear = 365 / day
+        const composedInterestRateYear = periodInterestRate ** reinvestCountYear
+        const totalFeeYear = this.fee * reinvestCountYear
+
+        const yearRewardAtom =
+          this.amount * composedInterestRateYear - totalFeeYear - this.amount
+
+        // eslint-disable-next-line no-console
+        console.log('amount: ' + this.amount)
+
+        // eslint-disable-next-line no-console
+        console.log('periodInterestRate: ' + periodInterestRate)
+        // eslint-disable-next-line no-console
+        console.log('reinvestCount: ' + reinvestCount)
+        // eslint-disable-next-line no-console
+        console.log('composedInterestRate: ' + composedInterestRate)
+
+        // eslint-disable-next-line no-console
+        console.log('totalAtom: ' + monthRewardAtom)
+
+        items.push({
+          period: day + '日',
+          totalFee,
+          rewardByPeriod,
+          monthRewardAtom: monthRewardAtom < 0 ? 0 : monthRewardAtom,
+          totalFeeYear,
+          yearRewardAtom: yearRewardAtom < 0 ? 0 : yearRewardAtom,
+        })
+      }
+      return items
+    },
+    best() {
+      return Math.max(...this.items.map((o) => o.yearRewardAtom))
+    },
+  },
+  methods: {
+    itemClass(item) {
+      return item.yearRewardAtom === this.best ? 'best' : ''
+    },
+  },
+}
+</script>
+
+<style>
+.best {
+  background-color: rebeccapurple;
+}
+</style>
